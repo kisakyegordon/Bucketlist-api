7a67e4788666bb9c5dbd638fc61623b7c49c74ec
Brian Serumaga <bsk20111@hotmail.com>
2017-09-17 21:20:32 +0300

    Edits the model for the users

--- app/models.py
+++ app/models.py
@@ -2,7 +2,17 @@ from app import app, db, bcrypt
 
 
 class User(db.Model):
-    pass
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String(255), nullable=False)
+    lname = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable = False)
+    password = db.Column(db.String(255), nullable=False)
+    bucketlists = db.relationships('Bucketlist', backref = bucketlist, lazy = 'dynamic')
+
+    def __init__(self):
+        pass
+
 
 
 class Bucketlist(db.Model):
