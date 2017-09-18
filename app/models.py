f2db618b3882d5442d09a44452f84aad4cecb2c4
Brian Serumaga <bsk20111@hotmail.com>
2017-09-18 11:46:26 +0300

    Edited the bucketlist models and the item model

--- app/models.py
+++ app/models.py
@@ -1,14 +1,15 @@
 from app import app, db, bcrypt
 
 
+
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     fname = db.Column(db.String(255), nullable=False)
     lname = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable = False)
     password = db.Column(db.String(255), nullable=False)
-    bucketlists = db.relationships('Bucketlist', backref = 'bucketlist', lazy = 'dynamic')
+    bucketlists = db.relationships('Bucketlist', lazy='dynamic')
 
     def __init__(self, fname, lname, email, password):
         self.fname = fname
@@ -17,12 +18,25 @@ class User(db.Model):
         self.password = password
 
 
-
-
 class Bucketlist(db.Model):
-    pass
+    __tablename__ = 'bucketlists'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    userId = db.Column(db.Integer, db.ForeignKey('users.id'))
+    blistname = db.Column(db.String(255), nullable=False)
+    items = db.Column(db.relationships('BucketListItems', lazy="dynamic"), nullable=False)
+
+    def __init__(self, blistname):
+        self.blistname = blistname
 
 
 class BucketlistItem(db.Model):
-    pass
+    __tablename__ = 'Bucketlistitems'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    item_name = db.Column(db.String(255), nullable=False)
+    completed = db.Column(db.Boolean, default=False, nullable=False)
+    bucketlist_id = db.Column(db.Integer, db.ForeignKey('bucketlists.id'), nullable=False)
+
+    def __init__(self, item_name):
+        self.item_name = item_name
+
 
