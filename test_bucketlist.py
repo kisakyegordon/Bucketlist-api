a1544f50df070e2c50823dc0591c16be854ad1dd
Brian Serumaga <bsk20111@hotmail.com>
2017-10-03 15:48:46 +0300

    Fixed the bug in the test setup.

--- test_bucketlist.py
+++ test_bucketlist.py
@@ -5,17 +5,33 @@ from app import create_app, db
 
 class BucketlistTestCase(unittest.TestCase):
 
-    def setup(self):
+
+
+    def setUp(self):
         self.app = create_app(config_name='testing')
+        self.client = self.app.test_client()
         self.bucketlist = {'name': 'Go to Borabora for vacation'}
         with self.app.app_context():
             db.create_all()
-            self.client = self.app.test_client()
+
+
 
     def test_bucketlist_creation(self):
         res = self.client.post('/bucketlists/', data=self.bucketlist)
         self.assertEqual(res.status_code, 201, msg="The status code is wrong.")
         self.assertIn('Go to Borabora', str(res.data))
+
+    def test_api_can_get_all_bucketlists(self):
+        res = self.client.post('/bucketlists/', data=self.bucketlist)
+        self.assertEqual(res.status_code, 201)
+        res = self.client.get('/bucketlists/')
+        self.assertEqual(res.status_code, 200)
+        self.assertIn('Go to Borabora', str(res.data))
+
+    def tearDown(self):
+        with self.app.app_context():
+            db.session.remove()
+            db.drop_all()
 
 if __name__ == "__main__":
     unittest.main()
