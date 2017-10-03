4cf95269a12d0956d1ad1c6e333872cfb71808bc
Brian Serumaga <bsk20111@hotmail.com>
2017-10-03 23:26:07 +0300

    Added the files containing the blueprint configrations

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
 from app import db
+from flask_bcrypt import Bcrypt
+from datetime import datetime, timedelta
+import jwt
 
-'''
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -10,27 +13,59 @@ class User(db.Model):
 
     def __init__(self, email, password):
         self.email = email
-        self.password = password
+        self.password = Bcrypt().generate_password_hash(password).decode()
 
     def save(self):
         db.session.add(self)
         db.session.commit()
 
+    def password_is_valid(self, password):
+        pass
+
+    def generate_token(self, user_id):
+        try:
+            # set up a payload with an expiration time
+            payload = {
+                'exp': datetime.utcnow() + timedelta(minutes=5),
+                'iat': datetime.utcnow(),
+                'sub': user_id
+            }
+            # create the byte string token using the payload and the SECRET key
+            jwt_string = jwt.encode(
+                payload,
+                current_app.config.get('SECRET'),
+                algorithm='HS256'
+            )
+            return jwt_string
+
+        except Exception as e:
+            # return an error in string format if an exception occurs
+            return str(e)
+
     @staticmethod
-    def get_all(self):
-        return User.query.all()
+    def decode_token(token):
+        try:
+            # try to decode the token using our SECRET variable
+            payload = jwt.decode(token, current_app.config.get('SECRET'))
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            # the token is expired, return an error string
+            return "Expired token. Please login to get a new token"
+        except jwt.InvalidTokenError:
+            # the token is invalid, return an error string
+            return "Invalid token. Please register or login"
 
     def delete(self):
         db.session.delete(self)
         db.session.commit()
-'''
 
 class Bucketlist(db.Model):
     __tablename__ = 'bucketlists'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    #userId = db.Column(db.Integer, db.ForeignKey('users.id'))
+    userId = db.Column(db.Integer, db.ForeignKey('users.id'))
     name = db.Column(db.String(255), nullable=False)
-    items = db.relationship('BucketlistItem', order_by='BucketlistItem.id',  lazy='dynamic')
+    items = db.relationship(
+        'BucketlistItem', order_by='BucketlistItem.id', cascade="all, delete-orphan")
 
     def __init__(self, name):
         self.name = name
@@ -51,13 +86,13 @@ class Bucketlist(db.Model):
         self.blistname = blistname
         db.session.commit()
 
+
 class BucketlistItem(db.Model):
     __tablename__ = 'Bucketlistitems'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     item_name = db.Column(db.String(255), nullable=False)
     completed = db.Column(db.Boolean, default=False, nullable=False)
     bucketlist_id = db.Column(db.Integer, db.ForeignKey('bucketlists.id'), nullable=False)
-
 
     def __init__(self, item_name):
         self.item_name = item_name
