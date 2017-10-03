fcf891dd1f63810d262653f87c691034b1223afa
Brian Serumaga <bsk20111@hotmail.com>
2017-10-03 16:16:22 +0300

    Add tests for crud functionality for bucketlist

--- app/__init__.py
+++ app/__init__.py
@@ -43,4 +43,33 @@ def create_app(config_name):
             response = jsonify(results)
             response.status_code = 200
             return response
+
+    @app.route('/bucketlists/<int:id>', methods = ['GET', 'PUT', 'DELETE'])
+    def bucketlist_manipulation(id, **kwargs):
+        bucketlist = Bucketlist.query.filter_by(id=id).first()
+        if not bucketlist:
+            abort(404)
+        if request.method == 'DELETE':
+            bucketlist.delete()
+            return {
+                       "message": "bucketlist {} deleted successfully".format(bucketlist.id)
+                   }, 200
+        elif request.method == 'PUT':
+            name = str(request.data.get('name', ''))
+            bucketlist.name = name
+            bucketlist.save()
+            response = jsonify({
+                'id': bucketlist.id,
+                'name': bucketlist.name
+            })
+            response.status_code = 200
+            return response
+        else:
+            # GET
+            response = jsonify({
+                'id': bucketlist.id,
+                'name': bucketlist.name
+            })
+            response.status_code = 200
+            return response
     return app
